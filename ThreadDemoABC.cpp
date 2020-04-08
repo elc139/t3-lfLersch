70683f22666dc966f5772a42283f65e055c6db1b
Luiz Felipe Lersch <luizflersch@gmail.com>
2020-04-07 23:53:57 -0300

    Errado Certo?

--- ThreadDemoABC.cpp
+++ ThreadDemoABC.cpp
@@ -3,14 +3,15 @@
 #include <string>
 #include <thread>
 #include <mutex>
-
+#include <omp.h>
+using std::string;
+omp_lock_t writelock;
 class SharedArray {
 private:
    char* array;
-   int index;
    int size;
-   std::mutex mutex;
    bool usemutex;
+   int index;
 public:
    SharedArray(int n, bool use) : size(n), index(0), usemutex(use) {
       array = new char[size];
@@ -19,12 +20,16 @@ public:
    ~SharedArray() {
       delete[] array;
    }
-   void addChar(char c) {
-      if (usemutex) mutex.lock();
+   void addChar(char c, int index) {
+      if (usemutex) omp_set_lock(&writelock);
+      array[index] = c;
+      spendSomeTime();
+      if (usemutex) omp_unset_lock(&writelock);
+   }
+   void addChar2(char c) {
       array[index] = c;
       spendSomeTime();
       index++;
-      if (usemutex) mutex.unlock();
    }
    int countOccurrences(char c) {
       return std::count(array, array+size, c);
@@ -49,16 +54,13 @@ private:
    static const int nTimes = 20;
    SharedArray* array;
 public:
-   ArrayFiller(bool usemutex) {
-      array = new SharedArray(nThreads * nTimes, usemutex);
+   ArrayFiller(bool use_lock){//, int method, int cluster) {
+      array = new SharedArray(nThreads * nTimes, use_lock);
    }
-   void fillArrayConcurrently() {
-      std::thread t[nThreads];
-      for (int i = 0; i < nThreads; ++i)
-         t[i] = std::thread(&ArrayFiller::run, this, 'A'+i);
-      for (int i = 0; i < nThreads; ++i)
-         t[i].join();
+   void fillArrayConcurrently(int mode) {
+      ArrayFiller::run(mode);
    }
+
    void printStats() {
       std::cout << array->toString() << std::endl;
       for (int i = 0; i < nThreads; ++i)
@@ -70,29 +72,144 @@ public:
       delete array;
    }
 private:
-   void run(char c) {
-      for (int i = 0; i < nTimes; i++) {
-         array->addChar(c);
+   void run(int mode) {
+      int i;
+      int chunk = 5;
+      omp_init_lock(&writelock);
+      switch(mode){
+         case 1: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(static)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 2: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(static, chunk)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 3: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(dynamic)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 4: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(dynamic, chunk)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 5: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(guided)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 6: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(guided, chunk)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 7: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(runtime)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 8: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               #pragma omp for schedule(auto)
+               for (i = 0; i < nTimes*nThreads; i++) {
+                  array->addChar('A' + omp_get_thread_num(), i);
+               }
+            }
+            break;
+         case 9: 
+            #pragma omp parallel shared(array) private(i)
+            {
+               for (i = 0; i < nTimes; i++) {
+                  array->addChar2('A' + omp_get_thread_num());
+               }
+            }
+            break;
       }
-   }
+      
+   omp_destroy_lock(&writelock);
+}
 };
 
 
 int main() {
    std::cout << "Each thread should add its char to the array n times (n=20)" << std::endl;
    std::cout << "Correct results should total exactly nThreads*nTimes chars" << std::endl;
-   std::cout << "Case 1: no mutex, default scheduling (expecting wrong results)" << std::endl;
-   ArrayFiller m1(false);
-   m1.fillArrayConcurrently();
+
+   std::cout << "Case 1: static no chunk" << std::endl;
+   ArrayFiller m1(true);
+   m1.fillArrayConcurrently(1);
    m1.printStats();
-   
-   std::cout << "Case 2: same as case 1 (possibly getting different results due to scheduling)" << std::endl;
-   ArrayFiller m2(false);
-   m2.fillArrayConcurrently();
+
+   std::cout << "Case 2: static chunk = 5" << std::endl;
+   ArrayFiller m2(true);
+   m2.fillArrayConcurrently(2);
    m2.printStats();
-   
-   std::cout << "Case 3: using mutex (expecting correct results)" << std::endl;
+
+   std::cout << "Case 3: dynamic no chunk" << std::endl;
    ArrayFiller m3(true);
-   m3.fillArrayConcurrently();
+   m3.fillArrayConcurrently(3);
    m3.printStats();
+
+   std::cout << "Case 4: dynamic chunk = 5" << std::endl;
+   ArrayFiller m4(true);
+   m4.fillArrayConcurrently(4);
+   m4.printStats();
+
+   std::cout << "Case 5: guided no chunk" << std::endl;
+   ArrayFiller m5(true);
+   m5.fillArrayConcurrently(5);
+   m5.printStats();
+
+   std::cout << "Case 6: guided chunk = 5" << std::endl;
+   ArrayFiller m6(true);
+   m6.fillArrayConcurrently(6);
+   m6.printStats();
+
+   std::cout << "Case 7: runtime" << std::endl;
+   ArrayFiller m7(true);
+   m7.fillArrayConcurrently(7);
+   m7.printStats();
+
+   std::cout << "Case 8: auto" << std::endl;
+   ArrayFiller m8(true);
+   m8.fillArrayConcurrently(8);
+   m8.printStats();
+
+   std::cout << "Case 9: static no lock" << std::endl;
+   ArrayFiller m9(false);
+   m9.fillArrayConcurrently(9);
+   m9.printStats();
 }
